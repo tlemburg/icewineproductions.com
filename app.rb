4767c6a87902d2b5264c0b91874f5c20c62b44e4
Tyler Lemburg <tyler.lemburg@archrival.com>
2014-10-10 15:43:12 -0500

    added redirect to keep away from herokuapp.com

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'thin'
 
+before do
+  if request.host.include?('herokuapp')
+    redirect to('http://www.icewineproductions.com'), 301
+  end
+end
+
 get '/' do
+
   erb :index
 end
